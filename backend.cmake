d737158f08fcf1e382426aed3db109fd57cb6d66
Aghaei <avazaghaei@yahoo.com>
2025-12-19 14:21:31 +0330

    Add libgit2 include path to all backend files

--- backend.cmake
+++ backend.cmake
@@ -10,3 +10,6 @@ set(INCLUDE_DIRS_BACKEND
     ${CMAKE_SOURCE_DIR}/Src/Utilities/
     ${CMAKE_SOURCE_DIR}/Src
 )
+
+#Add libgit2 include path to all backend files
+include_directories(${LIBGIT2_INCLUDE_DIR})
