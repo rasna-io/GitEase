77ae2e05918c92457e0b7229e1eed2e6f8e69fc3
Avazaghaei <avazaghaei@yahoo.com>
2026-09-09 04:31:37 -0700

    feat: Emit git command for tag deletion

--- Src/Git/GitTag.cpp
+++ Src/Git/GitTag.cpp
@@ -102,6 +102,8 @@ GitResult GitTag::remove(const QString &name)
     if (error < 0)
         return GitResult(false);
 
+    emitGitCommand(QString("git tag -d %1").arg(quoteCommandArg(name)));
+
     emit tagsChanged();
     return GitResult(true);
 }
