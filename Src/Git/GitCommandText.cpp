831fc73e95291cc7c31a7512fa92ff56e1512872
Avazaghaei <avazaghaei@yahoo.com>
2026-09-21 08:10:35 -0700

    feat: add branch command builders to GitCommandText

--- Src/Git/GitCommandText.cpp
+++ Src/Git/GitCommandText.cpp
@@ -14,3 +14,40 @@ QString GitCommandText::quote(const QString &argument)
     escaped.replace("\"", "\\\"");
     return "\"" + escaped + "\"";
 }
+
+QString GitCommandText::createBranch(const QString &branchName, const QString &startPoint)
+{
+    if (startPoint.isEmpty())
+        return QString("git branch %1").arg(quote(branchName));
+
+    return QString("git branch %1 %2").arg(quote(branchName), quote(startPoint));
+}
+
+QString GitCommandText::createBranchAndCheckout(const QString &branchName, const QString &startPoint)
+{
+    if (startPoint.isEmpty())
+        return QString("git checkout -b %1").arg(quote(branchName));
+
+    return QString("git checkout -b %1 %2").arg(quote(branchName), quote(startPoint));
+}
+
+QString GitCommandText::deleteBranch(const QString &branchName)
+{
+    return QString("git branch -d %1").arg(quote(branchName));
+}
+
+QString GitCommandText::renameBranch(const QString &oldName, const QString &newName)
+{
+    return QString("git branch -m %1 %2").arg(quote(oldName), quote(newName));
+}
+
+QString GitCommandText::checkoutBranch(const QString &branchName)
+{
+    return QString("git checkout %1").arg(quote(branchName));
+}
+
+QString GitCommandText::checkoutCommit(const QString &commitHash)
+{
+    return QString("git checkout --detach %1").arg(quote(commitHash));
+}
+
