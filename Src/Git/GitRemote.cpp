fa29a0166626aa764e03870f0d1f43d1ccb9e134
Avazaghaei <avazaghaei@yahoo.com>
2026-09-21 08:32:09 -0700

    refactor: route setRemoteUrl through GitCommandText

--- Src/Git/GitRemote.cpp
+++ Src/Git/GitRemote.cpp
@@ -476,8 +476,7 @@ GitResult GitRemote::editRemote(const QString &oldName, const QString &newName, 
 
     const QString effectiveName = (!newName.isEmpty() && newName != oldName) ? newName : oldName;
     if (!newUrl.isEmpty()) {
-        emitGitCommand(QString("git remote set-url %1 %2")
-                           .arg(quoteCommandArg(effectiveName), quoteCommandArg(newUrl)));
+        emitGitCommand(GitCommandText::setRemoteUrl(effectiveName, newUrl));
     }
     
     return GitResult(true, activeRemoteName, "Remote configuration updated successfully.");
